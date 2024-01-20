69ced56120892f82ae09617775f0db7705e6d79b
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-01-20 10:11:14 +0530

    updated public ip fetching code

--- hackip/platforms/os.py
+++ hackip/platforms/os.py
@@ -3,32 +3,40 @@ import socket
 import re
 from hackip.platforms.base import BaseOperatingSystem
 
+
+class UnixLikeOS(BaseOperatingSystem):
+    def __init__(self, cuttly_api_key, advanced_scanning, os_name) -> None:
+        super().__init__(cuttly_api_key, advanced_scanning)
+        self.os_name = os_name
+
+    def _get_private_ip_address(self):
+        # The function creates a socket that connects to a remote server on the Internet
+        # This is used to determine the local machine's IP address on the network.
+        try:
+            # This is a dummy connection, it doesn't need to succeed
+            # 1.1.1.1 is a public DNS server provided by Cloudflare
+            # 80 is the standard HTTP port
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.connect(("1.1.1.1", 80))
+                return s.getsockname()[0]
+        except Exception as e:
+            return f"Error: {e}"
+
+
 class WindowsOS(BaseOperatingSystem):
     def __init__(self, cuttly_api_key, advanced_scanning) -> None:
         super().__init__(cuttly_api_key, advanced_scanning)
         self.os_name = "Windows Operating System"
-    
-    def _get_private_ip_address(self):
-        self.private_ip_addr = socket.gethostbyname_ex(self.hostname)[-1][-1]
-        return self.private_ip_addr
-    
-class LinuxOS(BaseOperatingSystem):
-    def __init__(self, cuttly_api_key, advanced_scanning) -> None:
-        super().__init__(cuttly_api_key, advanced_scanning)
-        self.os_name = "Linux Operating System"
 
     def _get_private_ip_address(self):
-        os.system("ip addr > out.txt")
-        f = open("out.txt", "r")
-        strings = re.findall(r"192.168.\d{1,3}.\d{1,3}", f.read())
-        self.private_ip_addr = strings[-2]
-        return self.private_ip_addr
-    
-class MacOS(BaseOperatingSystem):
+        return socket.gethostbyname(self.hostname)
+
+
+class LinuxOS(UnixLikeOS):
     def __init__(self, cuttly_api_key, advanced_scanning) -> None:
-        super().__init__(cuttly_api_key, advanced_scanning)
-        self.os_name = "Mac Operating System"
-    
-    def _get_private_ip_address(self):
-        self.private_ip_addr = socket.gethostbyname_ex(self.hostname)[-1][0]
-        return self.private_ip_addr
+        super().__init__(cuttly_api_key, advanced_scanning, "Linux Operating System")
+
+
+class MacOS(UnixLikeOS):
+    def __init__(self, cuttly_api_key, advanced_scanning) -> None:
+        super().__init__(cuttly_api_key, advanced_scanning, "Mac Operating System")
